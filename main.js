451143cbf1a3f6253e1c475b8ad661fbb94f1023
haley akchurin <hdakchurin@gmail.com>
2017-09-14 14:28:29 -0500

    first

---
+++ main.js
@@ -0,0 +1,32 @@
+// // call the plugin on the "#toc" element
+// 		$('#toc').fixedTOC({
+// 			menuOpens: 'click', // or 'mouseenter'
+// 			scrollSpeed: 1000,
+// 			menuSpeed: 300,
+// 			useSubMenus: false,
+// 			resetSubMenus: false,
+// 			topLinkWorks: true
+// 		});
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
