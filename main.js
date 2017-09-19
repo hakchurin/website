1c2360918d4b1582f834692ce1f5ad4fc91b897b
haley akchurin <hdakchurin@gmail.com>
2017-09-19 16:10:48 -0500

    work examples

--- main.js
+++ main.js
@@ -122,22 +122,21 @@ window.onclick = function(event) {
 // scrol
 
 
-
-// init controller
-window.addEventListener( 'load', function() {
-  var box = document.getElementById('box'),
-      docHeight = document.documentElement.offsetHeight;
-
-  window.addEventListener( 'scroll', function() {
-        // normalize scroll position as percentage
-    var scrolled = window.scrollY / ( docHeight - window.innerHeight ),
-        transformValue = 'scale('+scrolled+')';
-
-    box.style.WebkitTransform = transformValue;
-    box.style.MozTransform = transformValue;
-    box.style.OTransform = transformValue;
-    box.style.transform = transformValue;
-
-  }, false);
-
-}, false);
+//
+// window.addEventListener( 'load', function() {
+//   var box = document.getElementById('box'),
+//       docHeight = document.documentElement.offsetHeight;
+//
+//   window.addEventListener( 'scroll', function() {
+//         // normalize scroll position as percentage
+//     var scrolled = window.scrollY / ( docHeight + window.innerHeight ),
+//         transformValue = 'scale('+scrolled+')';
+//
+//     box.style.WebkitTransform = transformValue;
+//     box.style.MozTransform = transformValue;
+//     box.style.OTransform = transformValue;
+//     box.style.transform = transformValue;
+//
+//   }, false);
+//
+// }, false);
