52c2b87fc15edd889795bcbbfd6b320f24044b01
haley akchurin <hdakchurin@gmail.com>
2017-11-13 15:56:26 -0600

    dev project btn fix

--- main.js
+++ main.js
@@ -89,27 +89,6 @@ window.onclick = function(event) {
 })();
 
 
-$(document).ready(function(){
-    $('.dev-btn').click(function(){
-        init();
-    });
-});
-
-
-
-function show(elementID) {
-    var ele = document.getElementById(elementID);
-    if (!ele) {
-        alert("no such element");
-        return;
-    }
-    var pages = document.getElementsByClassName('page');
-    for (var i = 0; i < pages.length; i++) {
-        pages[i].style.display = 'block';
-    }
-    ele.style.display = 'block';
-
-}
 
 
 
