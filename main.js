4a8028b1d9382a428528cdd859baf5c4aaaf126d
haley akchurin <hdakchurin@gmail.com>
2017-12-01 16:55:12 -0600

    fixed dots

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ $(document).ready(function() {
 
 
 
-// 
+//
 // $(document).ready(function() {
 //     $("#motion-btn").click(function() {
 //         $("##motion-btn").css("text-align:center");
