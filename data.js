76aadbada268d9c3a140b079fb567246fc861ea6
haley akchurin <hdakchurin@gmail.com>
2017-12-06 14:55:18 -0600

    mgd project link target

--- data.js
+++ data.js
@@ -4,39 +4,52 @@
 
 
 var data = {
-  ponies: {
+  Clockwork: {
     title: 'Clockwork',
-    body: 'Practice with 3D modeling, with techniques focusing on lighting, sculpting, and movement. Created in Cinema 4D'
+    body: 'Practice with 3D modeling, with techniques focusing on lighting, sculpting, and movement. Created in Cinema 4D',
+    text:'view',
+    url:'https://vimeo.com/145183740'
   },
   saveTheBay: {
     title: 'Save the Bay',
     body: 'For this project at Expression College, I took on the creative director role and led myself and a team of designers to create a stop motion PSA for the Zero Trash, Zero Excuse campaign for Save the Bay, a  non-profit in the San Francisco Bay Area stressing the importance in eliminating the amount of trash consumers make, and above all making sure the trash doesn’t end up in the communities waterways.',
-    quote: ' "Save the Bay is the largest regional organization working to protect, restore and celebrate San Francisco Bay since 1961." '
+    quote: ' "Save the Bay is the largest regional organization working to protect, restore and celebrate San Francisco Bay since 1961." ',
+    text:'view',
+    url:'https://www.facebook.com/saveSFbay/videos/10153621892061335/'
   },
   sarahOliverHandbags: {
     title: 'Sarah Oliver Handbags',
-    body: 'In this client based, Advertising Concepts class, two digital filmmakers paired with a designer to create a short video showcasing their client and product. Our client was Sarah Oliver Handbags,  Sarah Oliver is the founder & CEO, designer and knitter extraordinaire. Each handbag is handknit in the USA and manufactured by The Purlettes + 1, a  group of men and women, averaging the age of 88 at The Redwoods Senior Retirement Community, in Northern California. Sarah Came to us wanting to rebrand her image to attract a younger demographic.'
+    body: 'In this client based, Advertising Concepts class, two digital filmmakers paired with a designer to create a short video showcasing their client and product. Our client was Sarah Oliver Handbags,  Sarah Oliver is the founder & CEO, designer and knitter extraordinaire. Each handbag is handknit in the USA and manufactured by The Purlettes + 1, a  group of men and women, averaging the age of 88 at The Redwoods Senior Retirement Community, in Northern California. Sarah Came to us wanting to rebrand her image to attract a younger demographic.',
+    text:'view',
+    url:'https://vimeo.com/145183740'
 },
-haley: {
+haleyReel: {
   title: 'Pegasus for life',
-  body: 'If unicorns aren\'t real, I will be so sad.'
+  body: 'If unicorns aren\'t real, I will be so sad.',
+  text:'view',
+  url:'https://vimeo.com/145183740'
 }
 };
 
 window.addEventListener('hashchange', function() {
   var dataToRender = location.hash.slice(1);
   renderContent(dataToRender);
-
 });
 
 function renderContent(whatContent) {
   document.querySelector('#content h1').textContent = data[whatContent].title;
   document.querySelector('#content main').textContent = data[whatContent].body;
   document.querySelector('#content quote').textContent = data[whatContent].quote;
-
+  document.querySelector('#content links').textContent = data[whatContent].text;
 
 
 }
+
+
+
+
+
+
 
 
 
--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-};
-
 
 
 // slide2
@@ -126,12 +105,6 @@ $(document).ready(function() {
 
 
 
-//
-// $(document).ready(function() {
-//     $("#motion-btn").click(function() {
-//         $("##motion-btn").css("text-align:center");
-//     });
-// });
 
 
 
